8d8fd2138f1ee7189d2da9e9e6899dc9939d7324
mlisle <mlisle@protonmail.com>
2020-11-11 10:49:04 -0800

    Update styling and improve game play

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ const players = {
     'null': 'white',
 }
 
+const numbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
 /*----- app's state (variables) -----*/
 let turn = 1;
@@ -12,7 +13,8 @@ let board;
 // let scores; // need to add scores function, possibly with reduce method??
 let winner;
 
-let numOfStones = board[pitIndex];
+
+
 
 /*----- cached element references -----*/
 // let currentPits = document.querySelectorAll('.pocket-a'); // can board equal this and pocket-a and pocket-b ??
@@ -25,57 +27,62 @@ let replayBtn = document.getElementById('replay-btn');
 
 
 /*----- event listeners -----*/
-document.getElementById('container').addEventListener('click', handleClick);
+document.getElementById('container').addEventListener('click', handleMove);
 document.getElementById('replay-btn').addEventListener('click', init);
 
 
 
 /*----- functions -----*/
-function handleClick(evt) {
+function handleMove(evt) {
+    // evt.target.id --> 'p1'
     if (evt.target.id === 'container') return;
     // if (board[evt.target.id]) return;
     // if (winner === true) return;
-    board[evt.target.id] = turn;
+    let pitIndex = parseInt(evt.target.id);
+    let numOfStones = board[pitIndex];
+    board[pitIndex] = 0;
+    while (numOfStones > 0) {
+        if(pitIndex + 1 > 12) {
+            pitIndex = 0;
+        } 
+        // else if(pitIndex === 0 && turn === -1) {
+        //     numOfStones += 0;
+        //     pitIndex += 1;
+        // } else if (pitIndex === 7 && turn === 1) {
+        //     numOfStones += 0;
+        //     pitIndex += 1;
+        // } 
+        else {
+            board[pitIndex + 1] += 1;
+            numOfStones -= 1;
+            pitIndex += 1;  
+        }
+    }
+    // if(pitIndex === 0 && turn === 1) {
+    //     turn = 1;
+    // }
+    // if(pitIndex === 7 && turn === -1) {
+    //     turn = -1;
+    // } else {
+    //     turn *= -1;
+    // }
     turn *= -1;
     render();
 }
 
-function handleMove(evt) {
-    let pitIndex = parseInt(evt.target.id);
-    // let numOfStones = board[pitIndex];
-    board[pitIndex] = 0;
-    while (numOfStones > 0) {
-        if(pitIndex + 1 > 11) {
-            pitIndex = 0;
-        } else {
-            board[pitIndex + 1] += 1;
-            numOfStones -= 1;
-            pitIndex += 1;
-        }
-        console.log(board);
-    }
-    if(pitIndex === 0 && turn === 1) {
-        turn *= -1 * -1;
-    }
-    if(pitIndex === 7 && turn === -1) {
-        turn *= -1 * -1;
-    }
-    render();
-}
 
 // function extraTurn() {
 //     if ()
 //      turn *= -1 * -1;
 // }
 
-function moveStones() {
-    for(i = 0; i <= numOfStones; i++) { // possibly just = needed
-        board[pitIndex] = 0;
-        board[i] += 1;
-    if(board[pitIndex] === 0 && turn === -1) {
-        board[0] += 0;
-    } else if (board[pitIndex] === 7 && turn === 1) {
-        board[7] += 0;
+function skipWells() {
+    //  loop over the board
+    for(i = 0; i <= board.length; i++) {
+    if(board[i] === 0 && turn === -1) {
+        board[i] += 0;
+    } else if (board[i] === 7 && turn === 1) {
+        board[i] += 0;
     }
   }
 }
@@ -84,46 +91,55 @@ init();
 
 function init() {
     board = [0, 3, 3, 3, 3, 3, 3, 0, 3, 3, 3, 3, 3, 3];
-    // currentPits = [3, 3, 3, 3, 3, 3];
-    // otherPits = [3, 3, 3, 3, 3, 3];
-    // currentWell = 0;
-    // otherWell = 0;
+
     winner = false;
 
     render();
 }
 
-
 function render() {
-    for(let i = 0; i < board.length; i++) {
-        squares[i].style.backgroundColor = images[board[i]];
+    board.forEach(function(numOfStones, idx) {
+        document.querySelectorAll('.pocket')[idx].innerHTML = numOfStones;
+    })
+};
 
-        // stones[i].innerText = numOfStones[board[i]];
-    } 
-    if(turn === 1) {
-        msgEl.innerText = "Next player's turn!";
-    } 
-    if(turn === -1) {
-        msgEl.innerText = "Next player's turn!";
-    }
-    // if(currentPits === 0) {
-    //     msgEl.innerText = "Player One wins!";
-    // } else if(otherPits === 0) {
-    //     msgEl.innerText = "Player Two wins!";
-    // }
-    if(turn === 1) {
-        stones.innerText = `${numOfStones}`;
-    }
-    // winning();
-    // if(numStones > {{otherPlayerStones}} && turn === 1) { // include the otherPlayerStones as a reduce method to tally the scores (scores function?)
-    //     msgEl.innerText = "Player One Wins!";
-    // }
-    // if(winner === true && turn === -1) {
-    //     msgEl.innerText = "Player Two Wins!";
-    // }
-}
+// function render() {
+//     for(let i = 0; i < board.length; i++) {
+//         // stones[i].innerText = board[i];
+//     } 
+//     if(turn === 1) {
+//         msgEl.innerText = "Next player's turn!";
+//     } 
+//     if(turn === -1) {
+//         msgEl.innerText = "Next player's turn!";
+//     }
+//     // if(currentPits === 0) {
+//     //     msgEl.innerText = "Player One wins!";
+//     // } else if(otherPits === 0) {
+//     //     msgEl.innerText = "Player Two wins!";
+//     // }
+//     if(turn === 1) {
+//         stones.innerText = `${numOfStones}`;
+//     }
+//     if(turn === -1) {
+//         stones.innerText = `${numOfStones}`;
+//     }
+//     // winning();
+//     // if(numStones > {{otherPlayerStones}} && turn === 1) { // include the otherPlayerStones as a reduce method to tally the scores (scores function?)
+//     //     msgEl.innerText = "Player One Wins!";
+//     // }
+//     // if(winner === true && turn === -1) {
+//     //     msgEl.innerText = "Player Two Wins!";
+//     // }
+// }
 
 // Above include click events to change numbers inside pits/pockets
+
+
+    // currentPits = [3, 3, 3, 3, 3, 3];
+    // otherPits = [3, 3, 3, 3, 3, 3];
+    // currentWell = 0;
+    // otherWell = 0;
 
 
 /*----- pseudo code pour moi -----*/
