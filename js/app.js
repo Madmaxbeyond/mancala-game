a44c3982c789f941ae319da0ed131c349b9e19a3
mlisle <mlisle@protonmail.com>
2020-11-11 22:31:23 -0800

    Complete functioning game logic, improve styling

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
 /*----- constants -----*/
 const players = {
-    '1': 'pink', 
-    '-1': 'green', 
+    '1': 'pink', // player-1
+    '-1': 'green', // player-2
     'null': 'white',
 }
-
-const numbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
 /*----- app's state (variables) -----*/
 let turn = 1;
@@ -17,13 +15,11 @@ let winner;
 
 
 /*----- cached element references -----*/
-// let currentPits = document.querySelectorAll('.pocket-a'); // can board equal this and pocket-a and pocket-b ??
-// let otherPits = document.querySelectorAll('.pocket-b');
-let stones = document.querySelectorAll('.pocket');
+let pockets = document.querySelectorAll('.pocket');
 let msgEl = document.getElementById('msg');
 let replayBtn = document.getElementById('replay-btn');
 
-// let stonesNumberB = document.querySelectorAll('.pocket-b');
+
 
 
 /*----- event listeners -----*/
@@ -31,115 +27,71 @@ document.getElementById('container').addEventListener('click', handleMove);
 document.getElementById('replay-btn').addEventListener('click', init);
 
 
-
 /*----- functions -----*/
 function handleMove(evt) {
-    // evt.target.id --> 'p1'
+     // could be replaced with a loop over an array that represents the  "off limits" numbers
+    if(turn === 1 && evt.target.id === '13') return;
+    if(turn === 1 && evt.target.id === '12') return;
+    if(turn === 1 && evt.target.id === '11') return;
+    if(turn === 1 && evt.target.id === '10') return;
+    if(turn === 1 && evt.target.id === '9') return;
+    if(turn === 1 && evt.target.id === '8') return;
+   
+    if(turn === -1 && evt.target.id === '1') return;
+    if(turn === -1 && evt.target.id === '2') return;
+    if(turn === -1 && evt.target.id === '3') return;
+    if(turn === -1 && evt.target.id === '4') return;
+    if(turn === -1 && evt.target.id === '5') return;
+    if(turn === -1 && evt.target.id === '6') return;
+
+    if(turn === 1 && evt.target.id === '7') return;
+    if(turn === 1 && evt.target.id === '0') return;
+    if(turn === -1 && evt.target.id === '7') return;
+    if(turn === -1 && evt.target.id === '0') return;
+   
     if (evt.target.id === 'container') return;
-    // if (board[evt.target.id]) return;
-    // if (winner === true) return;
+
     let pitIndex = parseInt(evt.target.id);
-    let numOfStones = board[pitIndex];
+    let numOfStones = board[pitIndex]
+
     board[pitIndex] = 0;
+    pitIndex += 1
+
     while (numOfStones > 0) {
-        if(pitIndex + 1 > 12) {
-            pitIndex = 0;
-        } 
-        // else if(pitIndex === 0 && turn === -1) {
-        //     numOfStones += 0;
-        //     pitIndex += 1;
-        // } else if (pitIndex === 7 && turn === 1) {
-        //     numOfStones += 0;
-        //     pitIndex += 1;
-        // } 
-        else {
-            board[pitIndex + 1] += 1;
-            numOfStones -= 1;
-            pitIndex += 1;  
+        if (pitIndex === 14){
+            pitIndex = 0
         }
+        // continue? to skip wells at 0 and 7
+        board[pitIndex] += 1;
+        numOfStones -= 1;
+        pitIndex += 1;
     }
-    // if(pitIndex === 0 && turn === 1) {
-    //     turn = 1;
-    // }
-    // if(pitIndex === 7 && turn === -1) {
-    //     turn = -1;
-    // } else {
-    //     turn *= -1;
-    // }
     turn *= -1;
     render();
-}
-
-
-// function extraTurn() {
-//     if ()
-//      turn *= -1 * -1;
-// }
-
-function skipWells() {
-    //  loop over the board
-    for(i = 0; i <= board.length; i++) {
-    if(board[i] === 0 && turn === -1) {
-        board[i] += 0;
-    } else if (board[i] === 7 && turn === 1) {
-        board[i] += 0;
-    }
-  }
+    console.log(board);
 }
 
 init();
 
 function init() {
     board = [0, 3, 3, 3, 3, 3, 3, 0, 3, 3, 3, 3, 3, 3];
-
+    turn = 1;
     winner = false;
-
     render();
 }
 
 function render() {
+    console.log(board)
     board.forEach(function(numOfStones, idx) {
-        document.querySelectorAll('.pocket')[idx].innerHTML = numOfStones;
+        pockets[idx].innerHTML = numOfStones;
     })
+    if(turn === 1) {
+        msgEl.innerText = "Player One's turn!";
+    } 
+    if(turn === -1) {
+        msgEl.innerText = "Player Two's turn!";
+    }
 };
-
-// function render() {
-//     for(let i = 0; i < board.length; i++) {
-//         // stones[i].innerText = board[i];
-//     } 
-//     if(turn === 1) {
-//         msgEl.innerText = "Next player's turn!";
-//     } 
-//     if(turn === -1) {
-//         msgEl.innerText = "Next player's turn!";
-//     }
-//     // if(currentPits === 0) {
-//     //     msgEl.innerText = "Player One wins!";
-//     // } else if(otherPits === 0) {
-//     //     msgEl.innerText = "Player Two wins!";
-//     // }
-//     if(turn === 1) {
-//         stones.innerText = `${numOfStones}`;
-//     }
-//     if(turn === -1) {
-//         stones.innerText = `${numOfStones}`;
-//     }
-//     // winning();
-//     // if(numStones > {{otherPlayerStones}} && turn === 1) { // include the otherPlayerStones as a reduce method to tally the scores (scores function?)
-//     //     msgEl.innerText = "Player One Wins!";
-//     // }
-//     // if(winner === true && turn === -1) {
-//     //     msgEl.innerText = "Player Two Wins!";
-//     // }
-// }
-
-// Above include click events to change numbers inside pits/pockets
-
-
-    // currentPits = [3, 3, 3, 3, 3, 3];
-    // otherPits = [3, 3, 3, 3, 3, 3];
-    // currentWell = 0;
-    // otherWell = 0;
 
 
 /*----- pseudo code pour moi -----*/
@@ -152,6 +104,3 @@ function render() {
 // gameStatus([3,3,4,2,3,5,1,3,3,3,3,3], 4); //=> [3,3,4,2,0,6,2,4,3,3,3,3]
 // gameStatus([0,0,3,0,0,6], 2); //=> [0,0,0,1,1,7]
 
-
-// Player turn ends if they did not end in the storage well:
-// return pit !== 6;
